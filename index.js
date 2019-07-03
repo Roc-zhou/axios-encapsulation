a74bc306f8de354b513507d8a3f8342ca5df21b8
Roc-zhou <13031902163@163.com>
2019-07-03 10:40:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const instance = axios.create({
   timeout: 10000,
   headers: {
     // name: 'demo'
+    //token: '' // TODO
   },
   // `transformRequest` 允许在向服务器发送前，修改请求数据
   // 只能用在 'PUT', 'POST' 和 'PATCH' 这几个请求方法
